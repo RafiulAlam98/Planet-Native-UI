d70d6d72bfe3543d456a4f70ad49475bda5cef2e
RafiulAlam <mrafiul.alam7@gmail.com>
2022-07-18 22:14:40 +0600

    google-font added

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { colors } from './src/theme/colors';
+import { typography } from './src/theme/typography';
 
 export default function App() {
+  const [loaded] = useFonts({
+    'Antonio-Medium': require('./assets/fonts/Antonio-Medium.ttf'),
+    'Spartan-Bold': require('./assets/fonts/LeagueSpartan-Bold.ttf'),
+    'Spartan-Regular': require('./assets/fonts/LeagueSpartan-Regular.ttf'),
+  });
+  if (!loaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: typography.bold, fontSize: 30 }}>
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +26,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.white,
     alignItems: 'center',
     justifyContent: 'center',
   },
